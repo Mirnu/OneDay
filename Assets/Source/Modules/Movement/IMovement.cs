379b74a4eed816c3f9a41cbe9331c69afef3a604
Mernya <kirillprofi007@gmail.com>
2024-05-03 21:49:11 +0300

    addde example how made your modules

--- Assets/Source/Modules/Movement/IMovement.cs
+++ Assets/Source/Modules/Movement/IMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IWalk
+public interface IMovement
 {
     public void setFreeze(bool statement);
 }
---
+++ Assets/Source/Modules/Movement/MovementProxy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movement
+{
+    public class MovementProxy : IMovement //caching, change request and etc
+    {
+        public void setFreeze(bool statement)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
