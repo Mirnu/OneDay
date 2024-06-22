6b441e83e868386d9b283bd3668eef349b95a819
ZuzAser80 <dde4th@gmail.com>
2024-06-22 13:41:16 +0500

    i hate minigames

--- Assets/Source/Modules/Minigame/MinigameManager.cs
+++ Assets/Source/Modules/Minigame/MinigameManager.cs
@@ -1,6 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
 
-
-public class MinigameManager {
+public class MinigameManager : IInitializable {
     private IMinigame currentMinigame;
     private MinigameView currentView;
+    public Dictionary<IMinigame, GameObject> MinigamePrefabDictionary = new Dictionary<IMinigame,GameObject>();
+
+    public void Initialize()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void init(IMinigame minigame, MinigameView view) {
+        currentView = view;
+        currentMinigame = minigame;
+    }
 }
--- Assets/Source/Modules/Minigame/MinigameView.cs
+++ Assets/Source/Modules/Minigame/MinigameView.cs
@@ -6,7 +6,9 @@ public class MinigameView : MonoBehaviour, IInitializable {
 
     private GameObject currentMinigamePrefab;
 
-    public void ChangeMinigamePrefab(GameObject minigamePrefab) {}
+    public void ChangeMinigamePrefab(GameObject minigamePrefab) {
+        currentMinigamePrefab = minigamePrefab;
+    }
 
     public void Initialize()
     {
